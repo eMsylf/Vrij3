47aa10777bfbabc007d9258fe6406520788400c5
CupcakeMyDucktape <36536272+CupcakeMyDucktape@users.noreply.github.com>
2020-09-24 16:18:08 +0200

    Addition to a prototype scene

--- Assets/Dungeon/Room.cs
+++ Assets/Dungeon/Room.cs
@@ -4,25 +4,25 @@ using UnityEngine;
 
 public class Room : MonoBehaviour
 {
-    public Transform CameraViewPoint;
-    public GameObject PhysicalRoom;
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.GetComponent<PlayerController>() == null)
-            return;
-        //Debug.Log("Entered " + name, this);
-        //Debug.Log("Activate " + name, this);
-        PhysicalRoom?.SetActive(true);
-        //Debug.Log("Set camera position to " + name, this);
-        Camera.main.transform.position = CameraViewPoint.position;
-        Camera.main.transform.rotation = CameraViewPoint.rotation;
-    }
+    //public Transform CameraViewPoint;
+    //public GameObject PhysicalRoom;
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.GetComponent<PlayerController>() == null)
+    //        return;
+    //    //Debug.Log("Entered " + name, this);
+    //    //Debug.Log("Activate " + name, this);
+    //    PhysicalRoom?.SetActive(true);
+    //    //Debug.Log("Set camera position to " + name, this);
+    //    Camera.main.transform.position = CameraViewPoint.position;
+    //    Camera.main.transform.rotation = CameraViewPoint.rotation;
+    //}
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.GetComponent<PlayerController>() == null)
-            return;
-        //Debug.Log("Left " + name + ". Deactivate.", this);
-        PhysicalRoom?.SetActive(false);
-    }
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    if (other.GetComponent<PlayerController>() == null)
+    //        return;
+    //    //Debug.Log("Left " + name + ". Deactivate.", this);
+    //    PhysicalRoom?.SetActive(false);
+    //}
 }
---
+++ Assets/OnHitDeath.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnHitDeath : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerController playerController = other.attachedRigidbody.GetComponent<PlayerController>();
+
+        if (playerController != null)
+        {
+            playerController.Die();
+        }
+    }
+}
