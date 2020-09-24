d7041b6f737808a89c1c43dfba1cc1fa9a37ac22
eMsylf <bobjeltes@hotmail.com>
2020-09-24 13:58:38 +0200

    Additions - Statistic Meter prefab - Attacks and dodges now consume stamina, and can't be performed if insufficient stamina is present - Stamina recharges after being used. After using stamina, it takes a while before stamina starts recovering

--- Assets/Combat/Fighter.cs
+++ Assets/Combat/Fighter.cs
@@ -7,6 +7,8 @@ namespace Combat
     {
         public Statistic Health;
         public Statistic Stamina;
+        public StaminaRecharge staminaRecharge;
+
         public int TouchDamage = 0;
 
         public List<GameObject> DeathObjects = new List<GameObject>();
@@ -23,6 +25,39 @@ namespace Combat
                 Health.SetCurrent(Health.max);
             if (Stamina.max != 0 && Stamina.syncCurrentToMax)
                 Stamina.SetCurrent(Stamina.max);
+
+            Stamina.OnUse += () => staminaRecharge.windup = 0f;
+            Stamina.OnUse += () => staminaRecharge.recharge = 0f;
+        }
+
+
+        private void Update()
+        {
+            ManageStaminaRecharge();
+        }
+
+        void ManageStaminaRecharge()
+        {
+            if (Stamina.Get() < Stamina.max)
+            {
+                if (staminaRecharge.windup < staminaRecharge.staminaRechargeWindupTime)
+                {
+                    staminaRecharge.windup += Time.unscaledDeltaTime;
+
+                }
+                else
+                {
+                    if (staminaRecharge.recharge < staminaRecharge.staminaRechargeTime)
+                    {
+                        staminaRecharge.recharge += Time.unscaledDeltaTime;
+                    }
+                    else
+                    {
+                        Stamina.SetCurrent(Stamina.current + 1);
+                        staminaRecharge.recharge = 0f;
+                    }
+                }
+            }
         }
 
         public void Attack()
@@ -52,10 +87,16 @@ namespace Combat
             Debug.Log(damageSource.name + " hit enemy " + name + " for " + damageTaken + " damage. New health: " + Health.current, this);
             TakeDamage(damageTaken);
         }
-
-        public void UseStamina(int amount)
+        [System.Serializable]
+        public class StaminaRecharge
         {
-            Stamina.SetCurrent(Mathf.Clamp(Stamina.current - amount, 0, Stamina.max));
+            [Tooltip("Time it takes for a stamina point to recover")]
+            public float staminaRechargeTime = 1f;
+            [Tooltip("Time between stamina usage and recovery")]
+            public float staminaRechargeWindupTime = 1f;
+            internal float recharge = 0f;
+            internal float windup = 0f;
         }
     }
+    
 }
