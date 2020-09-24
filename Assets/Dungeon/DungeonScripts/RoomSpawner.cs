ec13ec0980f544a63e9d11e6bcab30db51c111bb
CupcakeMyDucktape <36536272+CupcakeMyDucktape@users.noreply.github.com>
2020-09-24 11:22:10 +0200

    Fixed: Rooms were at the wrong position so their spawnpoints were off

--- Assets/Dungeon/DungeonScripts/RoomSpawner.cs
+++ Assets/Dungeon/DungeonScripts/RoomSpawner.cs
@@ -18,42 +18,49 @@ public class RoomSpawner : MonoBehaviour
     void Start()
     {
         templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplatesScript>();
-        Invoke("Spawn", 0.1f);
+        Invoke("Spawn", 1f);
     }
 
     void Spawn()
     {
-        if (spawned == false)
+        for (int i = 0; i < 10; i++)
         {
-            if (openingDirection == 1) {
-                // Need to spawn a room with a TOP door
-                rand = Random.Range(0, templates.topRooms.Length);
-                Instantiate(templates.topRooms[rand], transform.position, templates.topRooms[rand].transform.rotation);
-            }
+            if (spawned == false)
+            {
+                if (openingDirection == 1)
+                {
+                    // Need to spawn a room with a TOP door
+                    rand = Random.Range(0, templates.topRooms.Length);
+                    Instantiate(templates.topRooms[rand], transform.position, templates.topRooms[rand].transform.rotation);
+                }
 
-            else if (openingDirection == 2) {
-                // Need to spawn a room with a BOTTOM door
-                rand = Random.Range(0, templates.bottomRooms.Length);
-                Instantiate(templates.bottomRooms[rand], transform.position, templates.bottomRooms[rand].transform.rotation);
-            }
+                else if (openingDirection == 2)
+                {
+                    // Need to spawn a room with a BOTTOM door
+                    rand = Random.Range(0, templates.bottomRooms.Length);
+                    Instantiate(templates.bottomRooms[rand], transform.position, templates.bottomRooms[rand].transform.rotation);
+                }
 
-            else if (openingDirection == 3) {
-                // Need to spawn a room with a RIGHT door
-                rand = Random.Range(0, templates.rightRooms.Length);
-                Instantiate(templates.rightRooms[rand], transform.position, templates.rightRooms[rand].transform.rotation);
-            }
+                else if (openingDirection == 3)
+                {
+                    // Need to spawn a room with a RIGHT door
+                    rand = Random.Range(0, templates.rightRooms.Length);
+                    Instantiate(templates.rightRooms[rand], transform.position, templates.rightRooms[rand].transform.rotation);
+                }
 
-            else if (openingDirection == 4) {
-                // Need to spawn a room with a LEFT door
-                rand = Random.Range(0, templates.leftRooms.Length);
-                Instantiate(templates.leftRooms[rand], transform.position, templates.leftRooms[rand].transform.rotation);
+                else if (openingDirection == 4)
+                {
+                    // Need to spawn a room with a LEFT door
+                    rand = Random.Range(0, templates.leftRooms.Length);
+                    Instantiate(templates.leftRooms[rand], transform.position, templates.leftRooms[rand].transform.rotation);
+                }
+                spawned = true;
             }
-            spawned = true;
         }
     }
 
     private void OnTriggerEnter(Collider other){
-        if (other.CompareTag("SpawnPoint") && other.GetComponent<RoomSpawner>().spawned == true) {
+        if (other.CompareTag("DungeonSpawnpoint") && other.GetComponent<RoomSpawner>().spawned == true) {
             Destroy(gameObject);
         }
     }
