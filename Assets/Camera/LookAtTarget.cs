36ad0add060933253fa31a3d14e215705d33cde1
eMsylf <bobjeltes@hotmail.com>
2020-09-30 17:08:54 +0200

    Additions - LookAtTarget script cleanup - LookAtTarget demo scene

--- Assets/Camera/LookAtTarget.cs
+++ Assets/Camera/LookAtTarget.cs
@@ -20,8 +20,6 @@ public class LookAtTarget : MonoBehaviour
     {
         AlignRotation = default,
         Forward
-        //Up,
-        //Right
     }
     public EMethod Method = default;
     [Tooltip("Only takes effect when Forward is selected")]
@@ -40,20 +38,7 @@ public class LookAtTarget : MonoBehaviour
         }
 
         Vector3 targetPosition = GetTarget().transform.position;
-        Debug.DrawLine(transform.position, targetPosition, Color.white, 1f);
 
-        //switch (FacingSide)
-        //{
-        //    case EFacingSide.Forward:
-        //        if (StayUpright) targetPosition.y = transform.position.y;
-        //        break;
-        //    case EFacingSide.Up:
-        //        if (StayUpright) targetPosition.z = transform.position.z;
-        //        break;
-        //    case EFacingSide.Right:
-        //        if (StayUpright) targetPosition.x = transform.position.x;
-        //        break;
-        //}
         if (StayUpright) targetPosition.y = transform.position.y;
 
         lookVector = transform.position - targetPosition;
@@ -63,12 +48,6 @@ public class LookAtTarget : MonoBehaviour
             case EMethod.Forward:
                 transform.forward = lookVector;
                 break;
-            //case EMethod.Up:
-            //    transform.up = lookVector;
-            //    break;
-            //case EMethod.Right:
-            //    transform.right = lookVector;
-            //    break;
         };
     }
 }
