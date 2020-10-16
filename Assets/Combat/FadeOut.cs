ceb23d6d25394c7bff90e6cb11766c1a2539791f
Bob <bobjeltes@hotmail.com>
2020-10-16 17:06:56 +0200

    Additions - Health and stamina meters now fade out after a few seconds (Delay) within a few seconds (Duration)

---
+++ Assets/Combat/FadeOut.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class FadeOut : MonoBehaviour
+{
+    public bool PlayOnStart = true;
+    public float Delay = 2f;
+    public float Duration = 1f;
+
+    private List<float> startAlphaValues;
+    private List<float> GetStartAlphaValues()
+    {
+        if (startAlphaValues == null)
+        {
+            startAlphaValues = new List<float>();
+            foreach (Graphic graphic in GetComponentsInChildren<Graphic>())
+            {
+                startAlphaValues.Add(graphic.color.a);
+            }
+        }
+        return startAlphaValues;
+    }
+
+    private void Awake()
+    {
+        GetStartAlphaValues();
+    }
+
+    void Start()
+    {
+        if (PlayOnStart)
+            StartFadeOut();
+    }
+
+    public void StartFadeOut()
+    {
+        StopAllCoroutines();
+        StartCoroutine(FadeOutRoutine());
+    }
+
+    private IEnumerator FadeOutRoutine()
+    {
+        //Debug.Log("Start fade out with delay " + Delay);
+        yield return new WaitForSeconds(Delay);
+        Fade();
+    }
+
+    public void Fade()
+    {
+        //Debug.Log("Fade", this);
+        foreach (Graphic graphic in GetComponentsInChildren<Graphic>())
+        {
+            graphic.DOFade(0f, Duration);
+        }
+    }
+
+    public void ResetFade()
+    {
+        Graphic[] graphics = GetComponentsInChildren<Graphic>();
+        for (int i = 0; i < graphics.Length; i++)
+        {
+            Graphic graphic = graphics[i];
+            graphic.DOKill();
+            Color resetColor = graphic.color;
+            resetColor.a = GetStartAlphaValues()[i];
+            graphic.color = resetColor;
+        }
+    }
+}
