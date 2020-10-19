29ffe0e5423477528e32ea8ff626e3502734fb7e
eMsylf <bobjeltes@hotmail.com>
2020-10-19 14:56:24 +0200

    Additions - Grid Layout Group Auto Scaler: has a button that automatically scales the cell size, to fit the cells perfectly into 1 row or column, based on the "Axis" setting in the GridLayoutGroup component that it's attached to

---
+++ Assets/BobsToolkit/Scripts/Editor/GridLayoutGroupAutoScalerInspector.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+
+[CanEditMultipleObjects]
+[CustomEditor(typeof(GridLayoutGroupAutoScaler))]
+public class GridLayoutGroupAutoScalerInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        GridLayoutGroupAutoScaler targetScript = (GridLayoutGroupAutoScaler)target;
+        GridLayoutGroupAutoScaler[] targetScripts = new GridLayoutGroupAutoScaler[targets.Length];
+        for (int i = 0; i < targets.Length; i++)
+        {
+            targetScripts[i] = (GridLayoutGroupAutoScaler)targets[i];
+        }
+
+        //if (GUILayout.Button("Calculate remaining pixels"))
+        //{
+        //    targetScript.CalculateCellSize();
+        //}
+
+        EditorGUILayout.HelpBox("This component is designed to work for GridLayoutGroups that " +
+            "\nuse 1 row or column", 
+            MessageType.Info);
+
+        Undo.RecordObject(targetScript.GetComponent<GridLayoutGroup>(), "Fit into padding on selected axis");
+
+        if (GUILayout.Button("Fit into padding on selected axis"))
+        {
+            for (int i = 0; i < targetScripts.Length; i++)
+            {
+                EditorUtility.SetDirty(targetScripts[i]);
+                targetScripts[i].AutoScale();
+
+            }
+        }
+
+    }
+}
---
+++ Assets/BobsToolkit/Scripts/GridLayoutGroupAutoScaler.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(GridLayoutGroup))]
+[RequireComponent(typeof(RectTransform))]
+public class GridLayoutGroupAutoScaler : MonoBehaviour
+{
+    public Vector2 CalculateCellSize()
+    {
+
+        return CalculateCellSize(GetComponent<GridLayoutGroup>().startAxis);
+    }
+
+
+    public Vector2 CalculateCellSize(GridLayoutGroup.Axis axis)
+    {
+        GridLayoutGroup gridLayoutGroup = GetComponent<GridLayoutGroup>();
+        RectTransform rectTransform = GetComponent<RectTransform>();
+
+        float startingPixels;
+        float padding;
+        float totalSpacing;
+        int childCount = transform.childCount;
+        float pixelsRemaining = 0f;
+        float calculatedCellSize;
+        Vector2 newCellSize = gridLayoutGroup.cellSize;
+
+        switch (axis)
+        {
+            case GridLayoutGroup.Axis.Horizontal:
+                startingPixels = rectTransform.sizeDelta.x;
+                padding = gridLayoutGroup.padding.horizontal;
+                totalSpacing = gridLayoutGroup.spacing.x * childCount;
+
+                pixelsRemaining = startingPixels - (padding + totalSpacing);
+                calculatedCellSize =  pixelsRemaining / childCount;
+                newCellSize.x = calculatedCellSize;
+                break;
+            case GridLayoutGroup.Axis.Vertical:
+                startingPixels = rectTransform.sizeDelta.y;
+                padding = gridLayoutGroup.padding.vertical;
+                totalSpacing = gridLayoutGroup.spacing.y * childCount;
+
+                pixelsRemaining = startingPixels - (padding + totalSpacing);
+                calculatedCellSize =  pixelsRemaining / childCount;
+                newCellSize.y = calculatedCellSize;
+                break;
+        }
+
+
+        Debug.Log("Pixels remaining on the selected axis: " + pixelsRemaining + ". Calculated cell size: " + newCellSize);
+        return newCellSize;
+    }
+
+    public void AutoScale()
+    {
+        GridLayoutGroup gridLayoutGroup = GetComponent<GridLayoutGroup>();
+        
+        gridLayoutGroup.cellSize = CalculateCellSize();
+        gridLayoutGroup.CalculateLayoutInputHorizontal();
+        gridLayoutGroup.CalculateLayoutInputVertical();
+    }
+}
