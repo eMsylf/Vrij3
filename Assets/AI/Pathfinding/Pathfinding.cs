7aeaa3e41b4aeac3372548efe5d034921e1678ab
eMsylf <bobjeltes@hotmail.com>
2020-11-05 15:37:36 +0100

    WIP Pathfinding

---
+++ Assets/AI/Pathfinding/Pathfinding.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Pathfinding : MonoBehaviour
+{
+    private new Rigidbody rigidbody;
+    public Rigidbody Rigidbody
+    {
+        get
+        {
+            if (rigidbody == null)
+            {
+                rigidbody = GetComponent<Rigidbody>();
+            }
+            return rigidbody;
+        }
+    }
+
+    public enum Method
+    {
+        Waypoints
+    }
+    public Method method;
+
+    public float speed;
+
+    [SerializeField] protected WaypointManager waypointManager;
+    public WaypointManager WaypointManager
+    {
+        get
+        {
+            if (waypointManager == null)
+            {
+                Debug.LogError("No waypoint manager assigned to " + name, this);
+            }
+            return waypointManager;
+        }
+    }
+
+    public Transform currentWaypoint;
+    public int currentWaypointIndex;
+
+    private void Start()
+    {
+        currentWaypoint = WaypointManager.GetRandomWaypoint();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Rigidbody.AddForce((transform.position - currentWaypoint.position) * speed);
+    }
+}
---
+++ Assets/AI/Pathfinding/WaypointManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointManager : MonoBehaviour
+{
+    public Transform GetRandomWaypoint()
+    {
+        return transform.GetChild(Random.Range(0, transform.childCount - 1));
+    }
+
+    public Transform GetNextWaypoint(int current)
+    {
+        current++;
+        if (current >= transform.childCount)
+        {
+            current = 0;
+        }
+        return transform.GetChild(current);
+    }
+
+    public Transform GetPreviousWaypoint(int current)
+    {
+        current--;
+        if (current < 0)
+        {
+            current = transform.childCount - 1;
+        }
+        return transform.GetChild(current);
+    }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmosSelected()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Gizmos.DrawLine(transform.GetChild(i).position, GetNextWaypoint(i).position);
+        }
+    }
+#endif
+}
