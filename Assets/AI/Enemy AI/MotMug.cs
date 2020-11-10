352e3e115291a56798b47f596f29c45efd52eddd
Bob <bobjeltes@hotmail.com>
2020-11-10 18:00:07 +0100

    Additions - Motmug movement jitter: the Motmug now has a Vector3 field where a force can be defined. A random force will be continuously applied to the rigidbody using these values

--- Assets/AI/Enemy AI/MotMug.cs
+++ Assets/AI/Enemy AI/MotMug.cs
@@ -15,9 +15,53 @@ public class MotMug : Enemy
         return pathfinding;
     }
 
+    public bool movementJitter = true;
+    public Vector3 JitterAmount = Vector3.one;
+
     private void Update()
     {
         ManagePathfinding();
+    }
+
+    private void FixedUpdate()
+    {
+        MovementJitter();
+    }
+
+    private void MovementJitter()
+    {
+        if (!movementJitter)
+        {
+            return;
+        }
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (rb == null)
+            return;
+
+        rb.AddForce(RandomVector3(JitterAmount));
+    }
+
+    public Vector3 RandomVector3()
+    {
+        return RandomVector3(1f);
+    }
+
+    public Vector3 RandomVector3(float bounds)
+    {
+        return RandomVector3(bounds, bounds, bounds);
+    }
+
+    public Vector3 RandomVector3(Vector3 bounds)
+    {
+        return RandomVector3(bounds.x, bounds.y, bounds.z);
+    }
+
+    public Vector3 RandomVector3(float x, float y, float z)
+    {
+        return new Vector3(
+            Random.Range(-x, x),
+            Random.Range(-y, y),
+            Random.Range(-z, z));
     }
 
     private void ManagePathfinding()
