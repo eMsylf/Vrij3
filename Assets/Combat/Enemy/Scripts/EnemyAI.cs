ae2c33f7dc7ec4c75d20df0252ed65c374097c5f
Bob <bobjeltes@hotmail.com>
2020-11-17 16:05:44 +0100

    Changes - Commented out debug.log statements from enemy AI

--- Assets/Combat/Enemy/Scripts/EnemyAI.cs
+++ Assets/Combat/Enemy/Scripts/EnemyAI.cs
@@ -99,12 +99,12 @@ public class EnemyAI : MonoBehaviour
     {
         if (state == States.Idle)
         {
-            Debug.Log("Already in idle state");
+            //Debug.Log("Already in idle state");
             return;
         }
 
         idleTimeCurrent = Random.Range(IdleTime.x, IdleTime.y);
-        Debug.Log("To Idle for " + idleTimeCurrent);
+        //Debug.Log("To Idle for " + idleTimeCurrent);
         state = States.Idle;
         Pathfinding.enabled = false;
     }
@@ -122,7 +122,7 @@ public class EnemyAI : MonoBehaviour
 
     public void ToWander()
     {
-        Debug.Log("Transition to Wander");
+        //Debug.Log("Transition to Wander");
         state = States.Wander;
         Pathfinding.enabled = true;
         Pathfinding.useWaypointProximity = true;
@@ -139,7 +139,7 @@ public class EnemyAI : MonoBehaviour
             return;
         }
 
-        Debug.Log("Transition to Follow Single Target");
+        //Debug.Log("Transition to Follow Single Target");
         state = States.FollowSingleTarget;
         Pathfinding.enabled = true;
         Pathfinding.currentWaypoint = PlayerController.Instance.transform;
@@ -171,10 +171,10 @@ public class EnemyAI : MonoBehaviour
     {
         if (state == States.Attack)
         {
-            Debug.Log("Already in attack state");
+            //Debug.Log("Already in attack state");
             return;
         }
-        Debug.Log("Transition to Attack");
+        //Debug.Log("Transition to Attack");
         state = States.Attack;
         Pathfinding.enabled = false;
     }
