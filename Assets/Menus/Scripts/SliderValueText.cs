25b7a9905b1792b7304bd4bd4424ed39a9b45eed
Bob <bobjeltes@hotmail.com>
2020-12-21 12:33:02 +0100

    Visuals to option sliders, add lights to beam attack
    Additions
    - SliderValueText now has a selection for number formatting (none, percentage, zero to one, or a custom one)
    
    Changes
    - Option sliders now have a new visual
    - Option sliders have been simplified
    - Option sliders text can now be assigned to a separate object and no longer needs to be on the same object as the TextMeshPro component

--- Assets/Menus/Scripts/SliderValueText.cs
+++ Assets/Menus/Scripts/SliderValueText.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Runtime.Remoting.Messaging;
 
 namespace BobJeltes.Menu
 {
-    [RequireComponent(typeof(TextMeshProUGUI))]
     public class SliderValueText : MonoBehaviour
     {
         public Slider slider;
-        TextMeshProUGUI TMPComponent;
+        public TextMeshProUGUI TMPComponent;
         TextMeshProUGUI GetTMPComponent()
         {
             if (TMPComponent == null)
@@ -19,6 +19,35 @@ namespace BobJeltes.Menu
             }
             return TMPComponent;
         }
+
+        public enum NumberFormat
+        {
+            Raw,
+            Percentage,
+            ZeroToOne,
+            Custom
+        }
+        public NumberFormat numberFormat = NumberFormat.Percentage;
+
+        public string GetNumberFormat() => GetNumberFormat(numberFormat);
+
+        public string GetNumberFormat(NumberFormat format)
+        {
+            switch (format)
+            {
+                case NumberFormat.Raw:
+                    return string.Empty;
+                case NumberFormat.Percentage:
+                    return "##0%";
+                case NumberFormat.ZeroToOne:
+                    return "0.00";
+                case NumberFormat.Custom:
+                    return CustomFormat;
+                default:
+                    return string.Empty;
+            }
+        }
+        public string CustomFormat = "";
 
         [Tooltip("If auto-update is true, you DO NOT need to reference UpdateValue in the OnValueChanged of the Slider component.")]
         public bool autoUpdate = true;
@@ -39,7 +68,7 @@ namespace BobJeltes.Menu
 
         public void UpdateValue(float value)
         {
-            string formattedSliderValue = value.ToString("##0%");
+            string formattedSliderValue = value.ToString(GetNumberFormat());
             GetTMPComponent().text = formattedSliderValue; //TODO https://docs.microsoft.com/en-us/dotnet/standard/base-types/custom-numeric-format-strings
             //Debug.Log("Update value to " + formattedSliderValue, this);
         }
