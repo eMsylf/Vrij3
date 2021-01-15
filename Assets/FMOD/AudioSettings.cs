a333cc2d675572c74c45bb8d98c65788ae41ed9a
Bob <bobjeltes@hotmail.com>
2021-01-15 16:36:12 +0100

    Audio settings now work across scenes, fix double-return on back button

--- Assets/FMOD/AudioSettings.cs
+++ Assets/FMOD/AudioSettings.cs
@@ -1,69 +1,98 @@
 ﻿using BobJeltes.StandardUtilities;
+using FMODUnity;
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AudioSettings : Singleton<AudioSettings>
 {
     FMOD.Studio.EventInstance SFXVolumeTestEvent;
 
-    FMOD.Studio.Bus SFX;
-    FMOD.Studio.Bus Music;
-    FMOD.Studio.Bus Master;
+    
 
-    [Range(0f, 1f)]
-    public float defaultVolume = .5f;
-
-    public string sfxName = "SFX";
-    public string musicName = "Music";
-    public string masterName = "Master";
+    public AudioSetting sfx = new AudioSetting("SFX");
+    public AudioSetting music = new AudioSetting("Music");
+    public AudioSetting master = new AudioSetting("Master");
 
     private void Awake()
     {
-        SFX = FMODUnity.RuntimeManager.GetBus("bus:/" + sfxName);
-        Music = FMODUnity.RuntimeManager.GetBus("bus:/"+ musicName);
-        Master = FMODUnity.RuntimeManager.GetBus("bus:/");
+        sfx.audioBus = FMODUnity.RuntimeManager.GetBus("bus:/" + sfx.name);
+        music.audioBus = FMODUnity.RuntimeManager.GetBus("bus:/"+ music.name);
+        master.audioBus = FMODUnity.RuntimeManager.GetBus("bus:/");
 
-        InitVolume(sfxName);
-        InitVolume(musicName);
-        InitVolume(masterName);
+        sfx.Init();
+        music.Init();
+        master.Init();
     }
 
-    public void InitVolume(string keyName)
+    public void Set(AudioSetting setting, float value)
     {
-        if (!PlayerPrefs.HasKey(keyName))
-        {
-            PlayerPrefs.SetFloat(keyName, defaultVolume);
-            PlayerPrefs.Save();
-        }
-        else
-        {
-            SetMusic(PlayerPrefs.GetFloat(keyName));
-        }
+        setting.Set(value);
     }
 
     public void SetMaster(float value)
     {
         if (Instance == null) return;
-        Instance.Master.setVolume(value);
+        Instance.master.audioBus.setVolume(value);
 
-        //PlayerPrefs.SetFloat("master", value);
-        //PlayerPrefs.Save();
+        PlayerPrefs.SetFloat(Instance.master.name, value);
+        PlayerPrefs.Save();
     }
 
     public void SetMusic(float value)
     {
         if (Instance == null) return;
-        Instance.Music.setVolume(value);
+        Instance.music.audioBus.setVolume(value);
         
-        PlayerPrefs.SetFloat(musicName, value);
+        PlayerPrefs.SetFloat(Instance.music.name, value);
         PlayerPrefs.Save();
     }
 
     public void SetSFX(float value)
     {
         if (Instance == null) return;
-        Instance.SFX.setVolume(value);
+        Instance.sfx.audioBus.setVolume(value);
 
-        PlayerPrefs.SetFloat(sfxName, value);
+        PlayerPrefs.SetFloat(Instance.sfx.name, value);
         PlayerPrefs.Save();
+    }
+
+    [System.Serializable]
+    public class AudioSetting
+    {
+        public string name;
+        public Slider slider;
+        [Range(0f, 1f)]
+        public float defaultVolume = .5f;
+        internal FMOD.Studio.Bus audioBus;
+
+        public AudioSetting(string _name)
+        {
+            name = _name;
+        }
+
+        internal void Init()
+        {
+            if (!PlayerPrefs.HasKey(name))
+            {
+                PlayerPrefs.SetFloat(name, defaultVolume);
+                PlayerPrefs.Save();
+            }
+            else
+            {
+                Set(PlayerPrefs.GetFloat(name));
+            }
+        }
+
+        internal void Set(float value)
+        {
+            audioBus.setVolume(value);
+            PlayerPrefs.SetFloat(name, value);
+            PlayerPrefs.Save();
+            if (slider == null)
+                Debug.LogWarning("Audio setting " + name + " has no slider assigned", Instance.gameObject);
+            else
+                slider.value = value;
+        }
     }
 }
