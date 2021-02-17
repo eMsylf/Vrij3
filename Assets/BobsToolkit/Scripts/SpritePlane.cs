4bb6d0f21a2acb0c04f7081d26c2afea4692b50d
Bob <bobjeltes@hotmail.com>
2021-02-17 13:23:16 +0100

    Made a double sprite plane script that makes the process of creating a sprite plane in 3D easier

--- Assets/BobsToolkit/Scripts/SpritePlane.cs
+++ Assets/BobsToolkit/Scripts/SpritePlane.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEditor.Rendering.PostProcessing;
@@ -8,25 +9,84 @@ using UnityEngine;
 [RequireComponent(typeof(MeshRenderer))]
 public class SpritePlane : MonoBehaviour
 {
-    public Sprite sprite;
+    public enum Orientation
+    {
+        XY,
+        XZ
+    }
+    public Orientation orientation;
     [Min(0.00001f)]
     public float scale = 1;
-    public bool invertX;
-    public bool invertY;
+    public bool invertHorizontal;
+    public bool invertVertical;
 
     private void OnValidate()
     {
         SetScale();
     }
 
+    public void UpdateScale(float scale, Orientation orientation, Mesh mesh, Material material, bool flipHorizontal, bool flipVertical)
+    {
+        SetMaterial(material);
+        SetMesh(mesh);
+        this.scale = scale;
+        this.orientation = orientation;
+        this.invertHorizontal = flipHorizontal;
+        this.invertVertical = flipVertical;
+        SetScale();
+    }
+
     public void SetScale()
     {
-        if (sprite == null)
+        Texture texture = GetComponent<MeshRenderer>().sharedMaterial.GetTexture("_MainTex");
+
+        if (texture == null)
+        {
+            Debug.Log("No texture found in the provided material");
             return;
+        }
+
         Vector3 newScale = Vector3.one;
-        newScale.x = sprite.rect.width * (invertX?-scale:scale);
-        newScale.y = sprite.rect.height * (invertY?-scale:scale);
+        switch (orientation)
+        {
+            case Orientation.XY:
+                newScale.x = texture.width * (invertHorizontal ? -scale : scale);
+                newScale.y = texture.height * (invertVertical ? -scale : scale);
+                break;
+            case Orientation.XZ:
+                newScale.x = texture.width * (invertHorizontal ? -scale : scale);
+                newScale.z = texture.height * (invertVertical ? -scale : scale);
+                break;
+            default:
+                break;
+        }
+
+        //if (sprite == null)
+        //    return;
+        //switch (orientation)
+        //{
+        //    case Orientation.XY:
+        //        newScale.x = sprite.rect.width * (invertHorizontal ? -scale : scale);
+        //        newScale.y = sprite.rect.height * (invertVertical ? -scale : scale);
+        //        break;
+        //    case Orientation.XZ:
+        //        newScale.x = sprite.rect.width * (invertHorizontal ? -scale : scale);
+        //        newScale.z = sprite.rect.height * (invertVertical ? -scale : scale);
+        //        break;
+        //    default:
+        //        break;
+        //}
 
         transform.localScale = newScale;
+    }
+
+    internal void SetMaterial(Material material)
+    {
+        GetComponent<MeshRenderer>().sharedMaterial = material;
+    }
+
+    internal void SetMesh(Mesh mesh)
+    {
+        GetComponent<MeshFilter>().sharedMesh = mesh;
     }
 }
