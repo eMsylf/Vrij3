7976940841863767c66b81b8011c23425a0b31f2
eMsylf <bobjeltes@hotmail.com>
2021-02-19 17:18:51 +0100

    Trying a different folder hierarchy to decrease top-level folder count

---
+++ Assets/Characters/Character.cs
@@ -0,0 +1,20 @@
+﻿using Combat;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Character : MonoBehaviour
+{
+    public CharacterProfile characterProfile;
+
+    [Header("Required components")]
+    public Stat health;
+
+    [Header("Optional components")]
+    public Stat stamina;
+    public Movement movement;
+
+    public UnityEvent die;
+    public UnityEvent revive;
+}
