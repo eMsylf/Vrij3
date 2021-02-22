a2102ea3c8fd3b6596011ab903d59b33ea759c3f
eMsylf <bobjeltes@hotmail.com>
2021-02-22 16:01:11 +0100

    WIP character stats prototyping

--- Assets/Characters/Character.cs
+++ Assets/Characters/Character.cs
@@ -6,12 +6,21 @@ using UnityEngine.Events;
 
 public class Character : MonoBehaviour
 {
+    [Header("Required components")]
     public CharacterProfile characterProfile;
 
-    [Header("Required components")]
-    public Stat health;
+    public struct Modifiers
+    {
+        public float energy;
+        public float health;
+        public float speed;
+        public float attackFrequency;
+        public float size;
+    }
+    public Modifiers modifiers;
 
     [Header("Optional components")]
+    public Stat health;
     public Stat stamina;
     public Movement movement;
 
--- Assets/Characters/CharacterProfile.cs
+++ Assets/Characters/CharacterProfile.cs
@@ -1,8 +1,43 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using BobJeltes.Attributes;
 using UnityEngine;
 
+
+[CreateAssetMenu(fileName = "New Character Profile", menuName = "Ranchy Rats/Character Profile")]
 public class CharacterProfile : ScriptableObject
 {
+    [Tooltip(
+        "Horizontal axis = energy\n" +
+        "Vertical axis = level")]
+    public AnimationCurve LevelEnergyCurve;
+    public float Health = 2;
+    public float Speed = 1;
+    public float AttackFrequency = 1;
+    public float Size = 1;
+    [Space]
+    public float testEnergy = 0;
+    [ReadOnly]
+    [SerializeField]
+    private int testLevel;
 
+    private void OnValidate()
+    {
+        TestLevel = Mathf.RoundToInt(LevelEnergyCurve.Evaluate(testEnergy));
+    }
+
+    public int TestLevel
+    {
+        get
+        {
+            return Mathf.RoundToInt(LevelEnergyCurve.Evaluate(testEnergy));
+        }
+        set
+        {
+            testLevel = value;
+        }
+    } 
+
+    //public float GetHealth(float energy)
+    //{
+    //    LevelCurve.Evaluate(energy);
+    //}
 }
