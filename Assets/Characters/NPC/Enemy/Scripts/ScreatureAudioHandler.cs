608104c1a1da070ef33c1b5a6af68ba85a33649c
Ruben Bergshoeff <ruben@runicsounds.com>
2021-05-10 13:25:21 +0200

    implemented screature attack audio

--- Assets/Characters/NPC/Enemy/Scripts/ScreatureAudioHandler.cs
+++ Assets/Characters/NPC/Enemy/Scripts/ScreatureAudioHandler.cs
@@ -6,11 +6,26 @@ using RunicSounds;
 public class ScreatureAudioHandler : MonoBehaviour
 {
     [SerializeField] private AudioEvent audioEventAttacking = default;
+    [SerializeField] private AudioParameter audioParameterAttackProgress = default;
+    [SerializeField] private float attackDuration = 0.75f;
     [SerializeField] private AudioEvent audioEventIsHit = default;
+
+    private float timeAttacking = 0;
+
+    private void Update()
+    {
+        if (audioEventAttacking.IsPlaying)
+        {
+            timeAttacking += Time.deltaTime;
+            audioEventAttacking.SetParameter(audioParameterAttackProgress, Mathf.Clamp01(timeAttacking / attackDuration));
+            Debug.Log(Mathf.Clamp01(timeAttacking / attackDuration));
+        }
+    }
 
     public void AttackStart()
     {
         audioEventAttacking.Play(gameObject, null);
+        timeAttacking = 0;
     }
 
     public void AttackEnd()
