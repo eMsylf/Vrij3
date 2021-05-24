2171bc83656406949eccd22d1f6d35ff8c76f9e1
Saphizzle <zoevandijke@gmail.com>
2021-05-24 14:54:15 +0200

    UPDATE anger dungeon zoe

---
+++ Assets/Animation/Scripts - State Machine Behaviours/DoorEnter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorEnter : MonoBehaviour
+{
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        anim.Play("AngerDoor_Open");
+    }
+    
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
